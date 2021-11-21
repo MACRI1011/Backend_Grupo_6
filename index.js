b269e8c99cc6a18d36fbded79a01f82eda22d45f
Mateo Soto Arango <mateo.sotoa@udea.edu.co>
2021-11-21 15:11:35 -0500

    Se conecta proyectos a base de datos

---
+++ index.js
@@ -0,0 +1,27 @@
+const usuario = require("./Sprint 2/Usuario/index.js");
+const proyecto = require("./Sprint 2/Proyecto/index.js")
+
+const { MongoClient } = require("mongodb");
+
+const url =
+  "mongodb+srv://erickAdmi:Salem14@gestionproyectos.hw0ed.mongodb.net/";
+const client = new MongoClient(url);
+const dbName = "Gestion-Proyectos";
+
+
+
+usuario.conexionUsuario()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
+proyecto.conexionProyecto()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());  
+
+
+
+
+
+
